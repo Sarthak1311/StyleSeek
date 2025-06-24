17b6fb49def14ee1f78359f3de8d6e4708aa9bce
sarthaktyagi1704@gmail.com <sarthaktyagi1704@gmail.com>
2025-06-24 13:14:50 +0530

    updating the file

--- clip_model.py
+++ clip_model.py
@@ -1,15 +1,16 @@
+# imorting the libraries 
+import os
 import clip
 import torch
 from PIL import Image
 
-# load model and preprocessing 
+# load model and preprocesing
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model ,preprocess = clip.load("ViT-B/32", device=device)
+model, preprocess = clip.load("ViT-B/32", device=device)
 
-def extract_image_features(image_path):
+def extract_image_feature(image_path):
     image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
     with torch.no_grad():
         features = model.encode_image(image)
-        features = features / features.norm(dim=-1, keepdim=True)
+        features = features/ features.norm(dim = -1,keepdim = True)
     return features.cpu().numpy().flatten()
-
